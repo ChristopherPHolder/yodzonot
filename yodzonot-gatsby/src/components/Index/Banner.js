6269fe109d874a11e56fdae3d84b401ae54c176e
Christopher <toholderandrews@gmail.com>
2021-07-25 18:12:16 +0200

    feat: improve style of banner and add room and acomodation to index

--- yodzonot-gatsby/src/components/Index/Banner.js
+++ yodzonot-gatsby/src/components/Index/Banner.js
@@ -5,27 +5,26 @@ import "../../assets/css/banner.css"
 
 const Banner = () => {
     return (
-        <div className="banner-container">
-            
+        <section className="banner-container">
             <StaticImage
             as="div"
             className="banner-image"
-            src="../../assets/images/background/IMG_4537.jpeg"
-            objectPosition="right top"
+            src="../../assets/images/background/main-house.webp"
+            objectPosition="50% 50%"
             />
 
             <div className="banner-centered">
                 <h1>
                     Text inside of banner
                 </h1>
-                <div className="btn-box">
+                <div className="btn-box banner-btn-box">
                     <Link to="/contact" className="default-btn">
                         <i className="flaticon-right"></i> 
                         Get Started <span></span>
                     </Link>
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
 export default Banner
--- yodzonot-gatsby/src/pages/index.js
+++ yodzonot-gatsby/src/pages/index.js
@@ -2,12 +2,16 @@ import React from "react"
 import Layout from "../components/App/Layout"
 import Navbar from "../components/App/Navbar"
 import Banner from "../components/Index/Banner"
+import Rooms from "../components/Index/Rooms"
+import Location from "../components/Index/Location"
 
 const IndexPage = () => {
   return (
     <Layout>
       <Navbar />
       <Banner />
+      <Location />
+      <Rooms />
     </Layout>
   )
 }
